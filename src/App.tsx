4a5511d266127e17b587d3adf3c86560131056ef
Stain-h <sangheon1646@naver.com>
2021-11-08 23:37:19 +0900

    add : 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { ErrorBoundary } from 'react-error-boundary';
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Add from './pages/Add'
+import Detail from './pages/Detail'
+import Edit from './pages/Edit'
+import NotFound from './pages/NotFound'
+import Error from './pages/Error'
+
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signin" component={Signin} />
+          <Route path="/add" component={Add} />
+          <Route path="/book/:id" component={Detail} />
+          <Route path="/edit/:id" component={Edit} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
